ce9f1921aca3e46fe8a4f331f7cb71eeaf9eed3f
osama zowaki <oussamazouaki4@gmail.com>
2021-11-06 18:05:23 +0100

    add insert builder

---
+++ insert.go
@@ -0,0 +1,58 @@
+package sqlbuilder
+
+type InsertBuilder struct {
+	*Builder
+	table   string
+	columns []string
+	values  [][]interface{}
+}
+
+func Insert(table string) *InsertBuilder {
+	b := &InsertBuilder{
+		Builder: NewBuilder(),
+		table:   table,
+		columns: []string{},
+		values:  [][]interface{}{},
+	}
+	return b
+}
+
+func (b *InsertBuilder) Columns(columns ...string) *InsertBuilder {
+	b.columns = columns
+	return b
+}
+
+func (b *InsertBuilder) Values(values ...interface{}) *InsertBuilder {
+	b.values = append(b.values, values)
+	return b
+}
+
+func (b *InsertBuilder) Query() (string, []interface{}) {
+	b.WriteString("INSERT INTO ")
+	b.WriteString(b.table)
+	b.WriteString(" (")
+
+	for i, c := range b.columns {
+		if i > 0 {
+			b.WriteString(",")
+		}
+		b.WriteString(c)
+	}
+
+	b.WriteString(") ")
+	b.WriteString("VALUES ")
+
+	for i, v := range b.values {
+		if i > 0 {
+			b.WriteString(",")
+		}
+		for j, u := range v {
+			if j > 0 {
+				b.WriteString(",")
+			}
+			b.WriteArg(u)
+		}
+	}
+
+	return b.String(), b.args
+}
