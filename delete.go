943bd5690f591755fbf60f941f40ff2767d9ee0a
osama zowaki <oussamazouaki4@gmail.com>
2021-11-07 23:33:50 +0100

    add delete query

---
+++ delete.go
@@ -0,0 +1,29 @@
+package sqlbuilder
+
+type DeleteBuilder struct {
+	*Builder
+	table string
+	where BuildFunc
+}
+
+func DeleteFrom(table string) *DeleteBuilder {
+	db := &DeleteBuilder{
+		Builder: NewBuilder(),
+		table:   table,
+	}
+	return db
+}
+
+func (db *DeleteBuilder) Where(cond BuildFunc) *DeleteBuilder {
+	db.where = cond
+	return db
+}
+
+func (db *DeleteBuilder) Query() (string, []interface{}) {
+	db.WriteString("DELETE FROM ")
+	db.WriteString(db.table)
+	db.WriteString(" WHERE ")
+	db.where(db.Builder)
+
+	return db.String(), db.args
+}
