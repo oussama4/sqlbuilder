512252f6760c0f8725b4b0172396172015b3b964
osama zowaki <oussamazouaki4@gmail.com>
2021-11-14 15:01:55 +0100

    buil other missing operators

--- op.go
+++ op.go
@@ -15,18 +15,29 @@ const (
 	OpAND // AND
 	OpOR  // OR
 	OpNot // NOT
+
+	OpIN      // IN
+	OpNotIN   // NOT IN
+	OpIsNULL  // IS NULL
+	OpNotNULL // IS NOT NULL
+	OpLIKE    // LIKE
 )
 
 var ops = [...]string{
-	OpEQ:  " = ",
-	OpNEQ: " <> ",
-	OpGT:  " > ",
-	OpGTE: " >= ",
-	OpLT:  " < ",
-	OpLTE: " <= ",
-	OpAND: " AND ",
-	OpOR:  " OR ",
-	OpNot: " NOT ",
+	OpEQ:      " = ",
+	OpNEQ:     " <> ",
+	OpGT:      " > ",
+	OpGTE:     " >= ",
+	OpLT:      " < ",
+	OpLTE:     " <= ",
+	OpAND:     " AND ",
+	OpOR:      " OR ",
+	OpNot:     " NOT ",
+	OpIN:      " IN ",
+	OpNotIN:   " NOT IN ",
+	OpIsNULL:  " IS NULL ",
+	OpNotNULL: " IS NOT NULL ",
+	OpLIKE:    " LIKE ",
 }
 
 // writeComp writes a comparaison operator
@@ -102,4 +113,64 @@ func Or(fns ...BuildFunc) BuildFunc {
 // Not builds a `NOT` operator
 func Not(fn BuildFunc) BuildFunc {
 	return buildLog(OpNot, fn)
+}
+
+// In builds an `IN` operator
+func In(col string, values ...interface{}) BuildFunc {
+	bf := func(b *Builder) {
+		b.WriteString(ops[OpIN])
+		b.WriteString("(")
+		for i, v := range values {
+			if i > 0 {
+				b.WriteString(",")
+			}
+			b.WriteArg(v)
+		}
+		b.WriteString(")")
+	}
+	return bf
+}
+
+// NotIn builds a `NOT IN` operator
+func NotIn(col string, values ...interface{}) BuildFunc {
+	bf := func(b *Builder) {
+		b.WriteString(ops[OpNotIN])
+		b.WriteString("(")
+		for i, v := range values {
+			if i > 0 {
+				b.WriteString(",")
+			}
+			b.WriteArg(v)
+		}
+		b.WriteString(")")
+	}
+	return bf
+}
+
+// IsNull builds an `IS NULL` operator
+func IsNull(col string) BuildFunc {
+	bf := func(b *Builder) {
+		b.WriteString(col)
+		b.WriteString(ops[OpIsNULL])
+	}
+	return bf
+}
+
+// NotNull builds an `IS NOT NULL` operator
+func NotNull(col string) BuildFunc {
+	bf := func(b *Builder) {
+		b.WriteString(col)
+		b.WriteString(ops[OpNotNULL])
+	}
+	return bf
+}
+
+// Like builds a `LIKE` operator
+func Like(col, pattern string) BuildFunc {
+	bf := func(b *Builder) {
+		b.WriteString(col)
+		b.WriteString(ops[OpLIKE])
+		b.WriteArg(pattern)
+	}
+	return bf
 }
