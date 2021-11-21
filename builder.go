51ab1b8110be11be3a26199ce8a98007618a3da7
osama zowaki <oussamazouaki4@gmail.com>
2021-11-21 18:43:50 +0100

    support custom sql dialects

--- builder.go
+++ builder.go
@@ -5,12 +5,20 @@ import (
 	"strings"
 )
 
+// Dialect names
+const (
+	Postgres = "postgres"
+	Sqlite   = "sqlite"
+	Mysql    = "mysql"
+)
+
 type BuildFunc func(*Builder)
 
 // Builder is where we put our built sql query
 type Builder struct {
 	*strings.Builder
 	args        []interface{}
+	dialect     string
 	paramsCount int
 }
 
@@ -19,6 +27,7 @@ func NewBuilder() *Builder {
 	b := &Builder{
 		Builder:     &strings.Builder{},
 		args:        []interface{}{},
+		dialect:     Postgres,
 		paramsCount: 0,
 	}
 	return b
@@ -38,7 +47,49 @@ func Expr(expr string, args ...interface{}) BuildFunc {
 // WriteArg adds an input argument to builder
 func (b *Builder) WriteArg(arg interface{}) {
 	b.paramsCount++
-	param := fmt.Sprintf("$%d", b.paramsCount)
+	param := "?"
+	if b.dialect == Postgres {
+		param = fmt.Sprintf("$%d", b.paramsCount)
+	}
 	b.WriteString(param)
 	b.args = append(b.args, arg)
+}
+
+// DialectBuilder is for building queries for a custom sql dialect
+type DialectBuilder struct {
+	dialect string
+}
+
+// Dialect creates a custom builder for the given sql dialect
+func Dialect(dialect string) DialectBuilder {
+	db := DialectBuilder{dialect: dialect}
+	return db
+}
+
+// Select creates a SelectBuilder for the configured dialect
+func (db DialectBuilder) Select(columns ...string) *SelectBuilder {
+	sb := Select(columns...)
+	sb.dialect = db.dialect
+	return sb
+}
+
+// Insert creates an InsertBuilder for the configured dialect
+func (db DialectBuilder) Insert(table string) *InsertBuilder {
+	ib := Insert(table)
+	ib.dialect = db.dialect
+	return ib
+}
+
+// Update creates an UpdateBuilder for the configured dialect
+func (db DialectBuilder) Update(table string) *UpdateBuilder {
+	ub := Update(table)
+	ub.dialect = db.dialect
+	return ub
+}
+
+// DeleteFrom creates a DeleteBuilder for the configured dialect
+func (db DialectBuilder) DeleteFrom(table string) *DeleteBuilder {
+	dlb := DeleteFrom(table)
+	dlb.dialect = db.dialect
+	return dlb
 }
