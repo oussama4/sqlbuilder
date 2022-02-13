2825bd7d1a64f925d303ae4411837d6b13ebef5a
osama zowaki <oussamazouaki4@gmail.com>
2022-02-13 19:25:20 +0100

    test returning clause

--- insert_test.go
+++ insert_test.go
@@ -21,6 +21,15 @@ func TestInsert(t *testing.T) {
 			wantQuery: "INSERT INTO products (title, description) VALUES ($1, $2), ($3, $4), ($5, $6)",
 			args:      []interface{}{"airpod", "airpod description", "airpod 2", "airpod 2 description", "airpod 3", "airpod 3 description"},
 		},
+		{
+			query: Insert("products").Columns("id, title, description").
+				Values("id1", "airpod", "airpod description").
+				Values("id2", "airpod 2", "airpod 2 description").
+				Values("id3", "airpod 3", "airpod 3 description").
+				Returning("id, title"),
+			wantQuery: "INSERT INTO products (id, title, description) VALUES ($1, $2, $3), ($4, $5, $6), ($7, $8, $9) RETURNING id, title",
+			args:      []interface{}{"id1", "airpod", "airpod description", "id2", "airpod 2", "airpod 2 description", "id3", "airpod 3", "airpod 3 description"},
+		},
 	}
 
 	for _, c := range cases {
